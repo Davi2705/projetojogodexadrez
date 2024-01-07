b80eb13fbaf7b31885f4b5c78163cef25cdd65c9
Davi Castro <davidecastro.silva@yahoo.com.br>
2024-01-07 17:59:54 -0300

    mudando o nome do metodo para movimentoPossivel

--- xadrez-console/obj/Debug/net8.0/xadrez-console.AssemblyInfo.cs
+++ xadrez-console/obj/Debug/net8.0/xadrez-console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("xadrez-console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f22e567e3951d66ccd80324804af4f4e44849c55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf7f111c03bfb10ec68c6a748001d1d90cfca4f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("xadrez-console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("xadrez-console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
